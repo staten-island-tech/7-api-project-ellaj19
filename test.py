8a5e399fd9e2cd3f040264dd481d6098ae4cefef
ellaj19 <ellaj19@nycstudents.net>
2025-12-09 10:17:23 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,21 +1,34 @@
 import requests
 
-def getcat(cat):
-    response = requests.get(f"https://api.thecatapi.com/v1/images/search{cat.lower()}")
+""" def getfruit(fruit):
+    response = requests.get(f"https://www.fruityvice.com/api/fruit/{fruit.lower()}")
     if response.status_code != 200:
         print("Error fetching data!")
         return None
-    for key, value in cat.items():
-        print(key, "→", value)
-
+    
     data = response.json()
-    return {
+    return data
+
+
+fruit = getfruit("strawberry")
+print(fruit) """
+
+def getcat(cat):
+    response = requests.get(f"https://api.edamam.com/api/recipes/v2/{cat.lower()}")
+    if response.status_code != 200:
+        print("Error fetching data!")
+        return None
+    
+    data = response.json()
+    return data
+
+
+meow = getcat("cookies")
+print(meow)
+
+""" return {
         "id": data["id"],
         "url": data["url"],
         "width": data["width"],
         "height": data["height"]
-    }
-
-
-cat = getcat("cv9")
-print(cat)
+    } """
