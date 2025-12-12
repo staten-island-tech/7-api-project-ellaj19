73e18c785b90a0feb1f4e1d13507df5199bcf2fe
ellaj19 <ellaj19@nycstudents.net>
2025-12-12 10:17:33 -0500

    Update test.py

--- test.py
+++ test.py
@@ -22,13 +22,11 @@ def getmario(nintendo):
     
     data = response.json()
     return pprint.pprint(data)
-""" 
-import tk
-window = tk.Tk()
-window.title("Message Reverser") 
-window.geometry("400x250") 
-window.resizable(False, False)
- """
+
+import tkinter
+window = tk
+
+
 
 meow = getmario("yoshi")
 print(meow)
