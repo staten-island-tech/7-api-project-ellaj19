32afa1dbf85541d389229f526996c100490bf1c3
ellaj19 <ellaj19@nycstudents.net>
2026-01-07 09:39:19 -0500

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,3 +1,11 @@
-import tkinter
-from tkinter import *
-x =  Tk()
+import requests
+def getmario(nintendo):
+    response = requests.get(f"https://www.amiiboapi.com/api/amiibo/?name={nintendo.lower()}")
+    if response.status_code != 200:
+        print("Error fetching data!")
+        return None
+    
+    data = response.json()
+    return data
+meow = getmario("yoshi")
+print(meow)
